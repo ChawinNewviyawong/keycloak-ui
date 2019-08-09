8a587a814403f65f06460b6bbaecd449bfca2ad3
gear <N.Chawin@gmail.com>
2019-08-09 10:48:18 +0700

    logout button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'keycloak-ui';
   port = location.port;
-
   
   constructor(
+    private cookieService: CookieService,
     private _router: Router
   ) { }
 
+  accessToken;
+
+  ngOnInit() {
+    this.accessToken = this.cookieService.get('accessToken');
+    this.prepareUserData();
+    this._router.events
+        .subscribe((event) => {
+          this.prepareUserData();
+        });
+  }
+
+  prepareUserData() {
+    this.accessToken = this.cookieService.get('accessToken');
+  }
+
+
   logout() {
     console.log(`[logout] kill cookie token`)
-
+    this.cookieService.deleteAll();
+    this._router.navigateByUrl('/login');
   }
 }
